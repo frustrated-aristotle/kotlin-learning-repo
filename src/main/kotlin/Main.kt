a09349803be1169d94da2f2d0f83264d331fb0ef
frustrated-aristotle <85283219+frustrated-aristotle@users.noreply.github.com>
2023-05-31 12:10:47 +0300

    Type Annotation for var typed variables How they work Why we need them

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,18 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    val a = 5
-    println(a)
+    /*  When a variable not defined with its type, after getting first value
+        variable will automatically detect the suitable type for the variable
+        and will give that type to variable.        */
+    var a = 100
+    /*  after that, if you try to give another type of value to the variable
+        you will get an error
+        a = 300000000 - This integer literal does not comfort to the expected type Int */
+
+    /*  TYPE ANNOTATION
+        We want to use var keyword to declare a variable but we can do want to prevent it becomes
+        an int
+        We use type annotation to prevent.
+    */
+    var b : Long = 100
+    b = 30000000000
 }
