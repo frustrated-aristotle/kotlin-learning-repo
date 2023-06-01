e4403f7b8a16f483400e51bd8e3599943a598d44
frustrated-aristotle <85283219+frustrated-aristotle@users.noreply.github.com>
2023-06-01 12:15:28 +0300

    We made a note keeper

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import com.sun.jdi.Value
+import java.util.HashMap
 import kotlin.reflect.typeOf
 
 fun main(args: Array<String>) {
@@ -32,5 +33,65 @@ fun main(args: Array<String>) {
     val intVal : Int = 10
     intVal.toDouble()
     val doubleA : Double = 5.12
-    println(doubleA)
+
+    //Array list
+
+    val javaArrayList = ArrayList<Any>()
+    javaArrayList.add(true)
+    javaArrayList.add(54)
+    javaArrayList.add("SS")
+    println("${javaArrayList.get(0)} and ${javaArrayList.get(1)}")
+
+    var firstInt = 10
+    var secondInt = 4
+
+    //Set can not take duplicate elements
+    val set1 = setOf<Any>(2,3,4, 4, 4, 4,4,55,true)
+    println(set1)
+
+    //We can not add elements to a set after declaration in that line
+    //set1.add() there is no such thing
+
+    //****************** HASH SET
+    val hashSet1 = hashSetOf<Any>()
+    //We do not need to initialize it
+    //It has add method
+    hashSet1.add(1)
+    hashSet1.add(true)
+    hashSet1.add(98)
+    hashSet1.add("string huh");
+    println(hashSet1);
+
+    //**************** MAPS
+
+    //A map works with key-value pair.
+    //var mapName = hashMapOf<KeyType, ValueType>()
+
+    var hashMap1 = hashMapOf<String, Int>()
+
+    //Adding elements to hash can be done with put function instead of add.
+    hashMap1.put("Key1", 1)
+    hashMap1.put("Key2", 2)
+
+    //Another way to do it:
+    //We can initialize the map when declaring it as follows:
+    //hashMapOf<KeyType, ValueType>(Key to Value, Key to Value)
+    var hashMap2 = hashMapOf<String, Int>("Key1" to 1, "Key2" to 2)
+
+    //Getting the element with its key from a has can be done as follows
+    //mapName.get("Key1")
+
+    println(hashMap1.get("Key1"))// prints out 1
+    println(hashMap1) // prints out {Key2=2, Key1=1}
+
+    var hashMap3 = hashMapOf<Int,ArrayList<String>>()
+
+    var notes = ArrayList<String>()
+    notes.add("first note")
+    notes.add("second note")
+    notes.add("third not")
+
+    hashMap3.put(1, notes)
+    println(hashMap3)
+
 }
